006536ee6880a371242eacf891b6bad792f34994
daniyal ali <daniyalali12568@gmail.com>
2024-02-14 03:14:04 +0500

    routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,26 @@
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Header from "./components/Header";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <BrowserRouter>
+      <Header/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/project" element={<Projects/>} />
+          <Route path="/signup" element={<Signup/>} />
+          <Route path="/signin" element={<Signin/>} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
-
 export default App;
