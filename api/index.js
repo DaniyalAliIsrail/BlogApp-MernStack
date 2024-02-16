893c10d248fd353d48bd9882ffac5a5fff12d9a9
daniyal ali <daniyalali12568@gmail.com>
2024-02-16 20:48:43 +0500

    db-connected

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config();
 
 const app = express();
 const PORT = 3000;
+ 
+mongoose.connect(process.env.DB_URI).then(()=>{
+    console.log("DATABASE CONNECTED");
+}).catch((err)=>{
+    console.log("error",err);
+})
 
 app.listen(PORT,()=>{
     console.log(`server is running on post ${PORT}`);
