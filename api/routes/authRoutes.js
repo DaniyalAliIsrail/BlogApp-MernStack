14ee163305578ea0c7a570fa8408c5fb706ce7b8
daniyal ali <daniyalali12568@gmail.com>
2024-09-03 03:29:54 +0500

    auth-signIn

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -1,7 +1,9 @@
 import express from "express";
-import signup from "../controller/authController.js";
+import {signup ,signin}  from "../controller/authController.js";
 const router = express.Router();
 
 router.post('/signup',signup)
+router.post('/signin',signin)
+
 
 export default router;
