987e173d4430ed69de8af86ee468cefb922789ac
daniyal ali <daniyalali12568@gmail.com>
2024-09-02 18:49:51 +0500

    footer

--- client/src/components/Myfooter.jsx
+++ client/src/components/Myfooter.jsx
@@ -1,7 +1,7 @@
 import { Footer } from "flowbite-react";
 import React from "react";
 import { Link } from "react-router-dom";
-import {BsFacebook, BsGithub, BsInstagram} from 'react-icons/bs';
+import { BsFacebook, BsGithub, BsInstagram } from "react-icons/bs";
 // import LOGO from "../assets/logp.png"
 
 const Myfooter = () => {
@@ -14,7 +14,7 @@ const Myfooter = () => {
               to="/"
               className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white "
             >
-              <span className="px-2 py-1   bg-gradient-to-r from-indigo-500 vis-purple-500 to-pink-500 rounded-lg text-white ">
+              <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 vis-purple-500 to-pink-500 rounded-lg text-white ">
                 Tenzy
               </span>
               {/* <img src={LOGO} /> */}
@@ -62,17 +62,20 @@ const Myfooter = () => {
             </div>
           </div>
         </div>
-        <Footer.Divider/>
+        <Footer.Divider />
         <div className="w-full sm:flex sm:items-center sm:justify-between">
-            <Footer.Copyright href="#" by="Tenzy's Blogs" year={new Date().getFullYear()}/>
-        
-        <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
-            <Footer.Icon href="#" icon={BsFacebook}/>
-            <Footer.Icon href="#" icon={BsInstagram}/>
-            <Footer.Icon href="#" icon={BsGithub}/>
-        
+          <Footer.Copyright
+            href="#"
+            by="Tenzy's Blogs"
+            year={new Date().getFullYear()}
+          />
+
+          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+            <Footer.Icon href="#" icon={BsFacebook} />
+            <Footer.Icon href="#" icon={BsInstagram} />
+            <Footer.Icon href="#" icon={BsGithub} />
+          </div>
         </div>
-      </div>
       </div>
     </Footer>
   );
