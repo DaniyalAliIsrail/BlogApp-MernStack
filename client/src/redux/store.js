af6d8041c039ea099b98acfd68dfc75460932369
daniyal ali <daniyalali12568@gmail.com>
2024-09-07 02:58:45 +0500

    storeandpersitredux-combine-reducer

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,37 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer from './user/userSlice'
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import userReducer from "./user/userSlice";
+import { persistReducer, persistStore,  } from "redux-persist";
+
+import storage from "redux-persist/lib/storage";
+
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+const persistConfig = {
+  key:'root',
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// export const store = configureStore({
+//   reducer: persistedReducer,
+//   middlewear: (getDefaultMiddleware) => {
+//     getDefaultMiddleware({ serializableCheck: false })
+//   },
+// });
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
-})
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST'], // Optionally, you can ignore specific actions
+        ignoredPaths: ['somePath'], // Optionally, you can ignore specific paths in the state
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
