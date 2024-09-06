bc74ac1853bbca16e814687e9ea00c6dbe362be0
daniyal ali <daniyalali12568@gmail.com>
2024-09-07 02:57:09 +0500

    sigin+ReduxPersist

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -57,7 +57,7 @@ const Signin = () => {
 
   return (
     <div className="min-h-screen mt-20">
-      <div className="flex p-3 max-w-3xl mx-auto flex-col md:items-center md:flex-row  gap-5 ">
+      <div className="flex flex-col p-3 max-w-3xl mx-auto  md:items-center md:flex-row  gap-5 ">
         <div className="flex-1">
           <Link to="/" className="font-bold dark:text-white text-4xl ">
             <span className="px-2 py-1 bg-gradient-to-r from-purple-500 vis-purple-500 to-blue-500 rounded-lg text-white ">
