a4abb815159ba6bbe777941eb27888e68c0e22b3
daniyal ali <daniyalali12568@gmail.com>
2025-03-04 03:22:45 +0500

    show-user-post

--- client/src/components/DashPost.jsx
+++ client/src/components/DashPost.jsx
@@ -1,9 +1,100 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Table } from "flowbite-react";
+import { Link } from "react-router-dom";
 const DashPost = () => {
+  const [getallPost, setgetallpost] = useState([]);
+  console.log(getallPost.updatedAt);
+  const { currentUser } = useSelector((state) => state.user);
+  useEffect(() => {
+    // Check if currentUser exists and has _id
+    if (!currentUser || !currentUser._id) {
+      return;
+    }
+    const fetchpost = async () => {
+      try {
+        const res = await fetch(`/api/post/getpost?userdId=${currentUser._id}`);
+        const data = await res.json();
+        if (res.ok) {
+          setgetallpost(data.posts);
+          console.log(data.posts);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    if (currentUser.isAdmin) {
+      fetchpost();
+    }
+  }, [currentUser._id]);
   return (
-    <div>DashPost</div>
-  )
-}
+    <div className="table-auto overflow-x-scroll md:mx-auto scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
+      {currentUser.isAdmin && getallPost.length > 0 ? (
+        <>
+          <Table hoverable className="shadow-md ">
+            <Table.Head>
+              <Table.HeadCell>Date updated</Table.HeadCell>
+              <Table.HeadCell>Post Image</Table.HeadCell>
+              <Table.HeadCell>Post Title</Table.HeadCell>
+              <Table.HeadCell>Category</Table.HeadCell>
+              <Table.HeadCell>Delete</Table.HeadCell>
+              <Table.HeadCell>
+                <span className="hidden md:block">Edit</span>
+              </Table.HeadCell>
+            </Table.Head>
+            {getallPost?.map((post, index) => (
+              <Table.Body>
+                <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                  <Table.Cell>
+                    {new Date(post.updatedAt).toLocaleDateString()}
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link to={`/post/${post.slug}`}>
+                      <img
+                        src={post.image}
+                        alt={post.title}
+                        className="w-20 h-10 object-cover bg-gray-500"
+                      />
+                    </Link>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link
+                      className="font-medium text-gray-900 dark:text-white"
+                      to={`/post/${post.slug}`}
+                    >
+                      {post.title}
+                    </Link>
+                  </Table.Cell>
+                  <Table.Cell>{post.category}</Table.Cell>
+                  <Table.Cell>
+                    <span
+                      onClick={() => {
+                        setShowModal(true);
+                        setPostIdToDelete(post._id);
+                      }}
+                      className="font-medium text-red-500 hover:underline cursor-pointer"
+                    >
+                      Delete
+                    </span>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link
+                      className="text-teal-500 hover:underline"
+                      to={`/update-post/${post._id}`}
+                    >
+                      <span>Edit</span>
+                    </Link>
+                  </Table.Cell>
+                </Table.Row>
+              </Table.Body>
+            ))}
+          </Table>
+        </>
+      ) : (
+        <p>You have no post yet</p>
+      )}
+    </div>
+  );
+};
 
-export default DashPost
+export default DashPost;
