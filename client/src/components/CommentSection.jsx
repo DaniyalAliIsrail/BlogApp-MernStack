d225e8db926f92c6912e9e49940fdfa0012c395a
daniyal ali <daniyalali12568@gmail.com>
2025-05-21 01:12:30 +0500

    comment-like-edit-delete-functionality

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,15 +1,19 @@
-import { Alert, Button, Textarea } from "flowbite-react";
-import React, { useState,  useEffect} from "react";
+import { Alert, Button, Modal, Textarea } from "flowbite-react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { UserComments } from "./UserComments";
+import { HiOutlineExclamationCircle } from "react-icons/hi";
+
 const CommentSection = ({ postId }) => {
-   const navigate = useNavigate();
+  const navigate = useNavigate();
   const { currentUser } = useSelector((state) => state.user);
   const [comment, setComment] = useState(""); //jo user comment k input pr lkh rha hay wo value store ho rahy hay
   const [commentError, setCommentError] = useState("");
   const [Commentsfetch, setCommentsfetch] = useState([]); // yaha pr jetna comment wo sb is array m la kr rakh do
-  console.log(Commentsfetch);
+  const [modal, setisModal] = useState(false);
+  const [commentToDelete, setcommentToDelete] = useState(null);
+  console.log("delteid", commentToDelete);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -27,74 +31,81 @@ const CommentSection = ({ postId }) => {
         }),
       });
       const data = await res.json();
-
       // console.log("post data",data);
       if (res.ok) {
-      //  setCommentsfetch(prevComments => [data, ...prevComments]);
-        setComment("")
+        //  setCommentsfetch(prevComments => [data, ...prevComments]);
+        setComment("");
         setCommentError("");
-      
       }
     } catch (error) {
       setCommentError(error.message);
     }
   };
-  // const handleLike = async (commentId) => {
-  //   try {
-  //     if (!currentUser) {
-  //      navigate("/sign-in");
-  //      return
-  //     }
-  //     const res = await fetch(`/api/comment/likeComment/${commentId}`, {
-  //       method: "PUT",
-  //     });
-  //     const data = await res.json();
-  //     console.log(data);
-  //     setCommentsfetch(Commentsfetch.map((comment)=>{
-  //      return comment._id === commentId ? {
-  //       ...comment,
-  //       likes:data.likes,
-  //       numberOfLikes:data.likes.length
-  //      }: comment
-  //     }))
-  //     console.log(data);
-  //   } catch (error) {
-  //     console.log(error.message);
-  //   }
-  // };
-const handleLike = async (commentId) => {
-  try {
-    if (!currentUser) {
-      navigate("/sign-in");
-      return;
+
+  const handleLike = async (commentId) => {
+    try {
+      if (!currentUser) {
+        navigate("/sign-in");
+        return;
+      }
+      const res = await fetch(`/api/comment/likeComment/${commentId}`, {
+        method: "PUT",
+      });
+      const data = await res.json();
+      console.log(data);
+      setCommentsfetch((prevComments) =>
+        prevComments.map((comment) =>
+          comment._id === commentId
+            ? {
+                ...comment,
+                likes: data.likes,
+                numberOfLikes: data.likes.length,
+              }
+            : comment
+        )
+      );
+    } catch (error) {
+      console.log(error.message);
     }
-    const res = await fetch(`/api/comment/likeComment/${commentId}`, {
-      method: "PUT",
-    });
-    const data = await res.json();
-    console.log(data);
-    setCommentsfetch(prevComments =>
-      prevComments.map(comment =>
-        comment._id === commentId
-          ? {
-              ...comment,
-              likes: data.likes,
-              numberOfLikes: data.likes.length,
-            }
-          : comment
+  };
+
+  const handleEdit = async (comment, editContent) => {
+    console.log("commentScetion josmjh nhy arha tha", comment);
+    setCommentsfetch(
+      Commentsfetch.map((c) =>
+        c._id === comment._id ? { ...c, content: editContent } : c
       )
     );
-  } catch (error) {
-    console.log(error.message);
-  }
-};
+  };
+
+  const handleDeleteModal = (id) => {
+    setisModal(true);
+    setcommentToDelete(id);
+  };
+  const handleDelete = async (commentId) => {
+    try {
+      if (!currentUser) {
+        navigate("/sign-in");
+      }
+      const res = await fetch(`/api/comment//commentdelete/${commentId}`, {
+        method: "DELETE",
+      });
+      if (res.ok) {
+        const data = await res.json();
+        setCommentsfetch(
+          Commentsfetch.filter((comment) => comment._id !== commentId)
+        );
+        setisModal(false);
+      }
+    } catch (error) {}
+  };
 
   useEffect(() => {
     const getComments = async () => {
       try {
         const res = await fetch(`/api/comment/getpostcomment/${postId}`);
         const data = await res.json();
-        console.log("Get comment==>",data);
+        console.log("Get comment==>", data);
         if (res.ok) {
           setCommentsfetch(data.comments);
         }
@@ -162,10 +173,41 @@ const handleLike = async (commentId) => {
               key={comments._id}
               comment={comments}
               onLike={handleLike}
+              onEdit={handleEdit}
+              onDelete={handleDeleteModal}
             />
           ))}
         </div>
       )}
+
+      <Modal
+        show={modal}
+        size="md"
+        onClose={() => setisModal(false)}
+        popup
+        className="z-[200]"
+      >
+        <Modal.Header />
+        <Modal.Body>
+          <div className="text-center">
+            <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
+            <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+              Are you sure you want to delete this post
+            </h3>
+            <div className="flex justify-center gap-4">
+              <Button
+                color="failure"
+                onClick={() => handleDelete(commentToDelete)}
+              >
+                Delete
+              </Button>
+              <Button onClick={() => setisModal(false)} color="gray">
+                No, cancel
+              </Button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
     </div>
   );
 };
--- client/src/components/UserComments.jsx
+++ client/src/components/UserComments.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { Alert, Button, Textarea, Modal } from "flowbite-react";
 import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import moment from "moment";
 import { FaThumbsUp } from "react-icons/fa";
 import { useSelector } from "react-redux";
-export const UserComments = ({ comment, onLike }) => {
-  const { currentUser } = useSelector((state)=> state.user);
+
+export const UserComments = ({ comment, onLike, onEdit, onDelete }) => {
+  const { currentUser } = useSelector((state) => state.user);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedContent, setEditedContent] = useState(comment.content);
   const [userdetailswithComments_userId, setuserdetailswithComments_userId] =
     useState();
+
   useEffect(() => {
     const getuser = async () => {
       const res = await fetch(`/api/user/getuser/${comment?.userId}`);
@@ -19,7 +24,33 @@ export const UserComments = ({ comment, onLike }) => {
     };
     getuser();
   }, [comment]);
-  // console.log(comment);
+
+  const handleEdit = () => {
+    setIsEditing(true);
+    setEditedContent(comment.content);
+  };
+
+  const handleSave = async () => {
+    try {
+      const res = await fetch(`/api/comment/commentupdate/${comment._id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          content: editedContent,
+        }),
+      });
+      if (res.ok) {
+        setIsEditing(false);
+        onEdit(comment, editedContent);
+      }
+      const data = await res.json();
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className="flex gap-x-3 gap-y-2 w-full mb-2 pb-2 border-b dark:border-gray-800">
       <div className="">
@@ -30,7 +61,7 @@ export const UserComments = ({ comment, onLike }) => {
       </div>
       <div className="w-full">
         <Link
-          className="text-xs text-gray-200 font-semibold light:text-blue-700  hover:underline"
+          className="text-black font-semibold text-xs hover:underline dark:text-white"
           to={"/dashboard?tab=profile"}
         >
           @{userdetailswithComments_userId?.username || "anonymous user"}
@@ -38,21 +69,84 @@ export const UserComments = ({ comment, onLike }) => {
         <span className="text-gray-400 mx-2 text-[0.66rem]">
           {moment(comment.createdAt).fromNow()}
         </span>
-        <p className="text-sm text-gray-300">{comment?.content}</p>
-        <div>
-          <button
-            className={`text-gray-400 hover:text-blue-500 ${currentUser && comment.likes.includes(currentUser._id) && '!text-blue-500'} `}
-            type="button"
-            onClick={() => onLike(comment._id)}
-          >
-            <FaThumbsUp className="text-sm" />
-          </button>
-          <p>
-            {
-              comment.numberOfLikes > 0 && comment.numberOfLikes + " " + (comment.numberOfLikes === 1 ? "like" : "likes")
-            }
-          </p>
-        </div>
+        {isEditing ? (
+          <>
+            <Textarea
+              className="mt-2"
+              placeholder="Add a Comments"
+              rows={3}
+              maxLength={200}
+              value={editedContent}
+              onChange={(e) => setEditedContent(e.target.value)}
+            />
+            <div className="flex justify-end  gap-3 my-2 text-xs">
+              <Button
+                onClick={handleSave}
+                type="button"
+                size="sm"
+                gradientDuoTone="purpleToBlue"
+              >
+                save
+              </Button>
+              <Button
+                onClick={() => setIsEditing(false)}
+                type="button"
+                size="sm"
+                gradientDuoTone="purpleToBlue"
+                outline
+              >
+                Cancel
+              </Button>
+            </div>
+          </>
+        ) : (
+          <>
+            <p className="text-sm text-gray-600  dark:text-gray-300">
+              {comment?.content}
+            </p>
+            <div className="flex items-center justify-start  gap-3 max-w-[10rem] ">
+              <button
+                className={`text-gray-400 hover:text-blue-500 ${
+                  currentUser &&
+                  comment.likes.includes(currentUser._id) &&
+                  "!text-blue-500"
+                } `}
+                type="button"
+                onClick={() => onLike(comment._id)}
+              >
+                <FaThumbsUp className="text-sm" />
+              </button>
+              <p className="text-sm">
+                {comment.numberOfLikes > 0 &&
+                  comment.numberOfLikes +
+                    " " +
+                    (comment.numberOfLikes === 1 ? "like" : "likes")}
+                {currentUser &&
+                  (currentUser._id == comment.userId ||
+                    currentUser.isAdmin) && (
+                    <>
+                      <button
+                        type="button"
+                        className="text-gray-400 p-1 hover:text-blue-500 text-sm"
+                        onClick={handleEdit}
+                      >
+                        Edit
+                      </button>
+                      <button
+                        type="button"
+                        className="text-gray-400 p-1 hover:text-red-500 text-sm"
+                        onClick={()=> onDelete(comment._id)}
+                      >
+                        Delete
+                      </button>
+                    </>
+                  )}
+              </p>
+            </div>
+          </>
+        )}
+
+        
       </div>
     </div>
   );
