52cfa2bbf776f3688c00244c9ee4826d047100b1
daniyal ali <daniyalali12568@gmail.com>
2025-05-22 01:58:18 +0500

    recent-article_post-functionality

---
+++ client/src/components/RecentPostCard.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const PostCard = ({postconent}) => {
+  return (
+    <div className="group relative h-[355px] overflow-hidden ">
+      <Link>
+        <img src={postconent.image} alt={postconent.title} className="h-[260px] w-full object-cover group-hover:h-[200px] transition-all duration-300" />
+      </Link>
+      <div className="flex flex-col gap-2 pt-2 px-2 mb-4" >
+         <p className="text-lg font-semibold line-clamp-2">{postconent.title}</p>
+         <span className="text-sm italic ">{postconent.category}</span>
+         <Link className="z-10 group-hover:bottom-0 absolute bottom-[-200px] left-0 right-0 border border-teal-400 text-teal-500 hover:text-gray-400 transition-all duration-300 p-2 mx-2 my-2 rounded text-center" to={`/post/${postconent.slug}`}>
+         Read Articles
+         </Link>
+      </div>
+    </div>
+  );
+};
--- client/src/pages/PostPageFullView.jsx
+++ client/src/pages/PostPageFullView.jsx
@@ -3,36 +3,55 @@ import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { CallToAction } from "../components/CallToAction";
 import CommentSection from "../components/CommentSection";
+import { PostCard } from "../components/RecentPostCard";
 
 const PostPageFullView = () => {
   const { postSlug } = useParams();
   const [post, setPost] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
+  const [recentPost, setRecentPost] = useState(null);
   console.log(post);
   useEffect(() => {
     const abortController = new AbortController(); //kam ko roknay ka controller
-    
+
     const fetchPost = async () => {
       try {
         const res = await fetch(`/api/post/getpost?slug=${postSlug}`, {
-          signal: abortController.signal
+          signal: abortController.signal,
         });
         const data = await res.json();
         setPost(data.posts[0]);
       } catch (error) {
-        if (error.name !== 'AbortError') {
+        if (error.name !== "AbortError") {
           setError(true);
         }
       } finally {
         setLoading(false);
       }
     };
-    
     fetchPost();
-    
+
     return () => abortController.abort();
   }, [postSlug]);
+
+  useEffect(() => {
+    const fetchRecentpost = async () => {
+      try {
+        const res = await fetch(`/api/post/getpost?limit=3`);
+        const data = await res.json();
+        if (res.ok) {
+          setRecentPost(data.posts);
+          console.log("data.post===>", data.posts);
+        }
+      } catch (error) {
+        setLoading(false);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchRecentpost();
+  }, []);
 
   if (loading) {
     return (
@@ -54,41 +73,58 @@ const PostPageFullView = () => {
 
   return (
     <>
-    <main className="max-w-3xl mx-auto p-3 min-h-screen">
-      <h1 className="text-3xl text-center font-serif max-w-2xl mx-auto p-3 lg:text-4xl">
-        {post?.title}
-      </h1>
-      
-      <Link to={`/search?category=${post.category}`}>
-        <Button color="gray" pill size={"xs"} className="mx-auto block my-2">
-          {post?.category}
-        </Button>
-      </Link>
-      
-      {post.image && (
-        <img
-          src={post?.image}
-          alt={post?.title}
-          className="w-full object-cover max-h-[600px mt-5]"
+      <main className="max-w-3xl mx-auto p-3 min-h-screen">
+        <h1 className="text-3xl text-center font-serif max-w-2xl mx-auto p-3 lg:text-4xl">
+          {post?.title}
+        </h1>
+
+        <Link to={`/search?category=${post.category}`}>
+          <Button color="gray" pill size={"xs"} className="mx-auto block my-2">
+            {post?.category}
+          </Button>
+        </Link>
+
+        {post.image && (
+          <img
+            src={post?.image}
+            alt={post?.title}
+            className="w-full object-cover max-h-[600px mt-5]"
+          />
+        )}
+
+        <div className="flex justify-between items-center my-2 text-gray-500 border-b-2 border-gray-200 w-full">
+          <span>{new Date(post.createdAt).toLocaleDateString()}</span>
+          <span>
+            {(post.content.replace(/<[^>]*>/g, "").length / 1000).toFixed(0)}{" "}
+            mins read
+          </span>
+        </div>
+
+        <div
+          className="pt-5 pb-4 post-content"
+          dangerouslySetInnerHTML={{ __html: post.content }}
         />
-      )}
-      
-      <div className="flex justify-between items-center my-2 text-gray-500 border-b-2 border-gray-200 w-full">
-        <span>{new Date(post.createdAt).toLocaleDateString()}</span>
-        <span>{(post.content.replace(/<[^>]*>/g, "").length / 1000).toFixed(0)} mins read</span>
-      </div>
-      
-      <div 
-        className="pt-5 pb-4 post-content" 
-        dangerouslySetInnerHTML={{__html: post.content}}
-      />
-    </main>
-    <div className="max-w-4xl mx-auto mb-4 px-3">
-    <CallToAction />
-    <CommentSection postId={post._id} />
-    </div>
-    <div>
-    </div>
+        <div className="max-w-4xl mx-auto mb-4 px-3">
+          <CallToAction />
+          <CommentSection postId={post._id} />
+        </div>
+      </main>
+
+      {/* //show recent articles */}
+       <div className="flex flex-col items-center px-4 py-6 max-w-7xl mx-auto ">
+          <h1 className="text-2xl font-semibold mb-6">Recent Articles</h1>
+          <div className="flex flex-wrap justify-center gap-6 w-full ">
+            {recentPost &&
+              recentPost.map((post) => (
+                <div 
+                  key={post._id}
+                  className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 border border-b-4  border-teal-400 rounded  "
+                >
+                  <PostCard postconent={post} />
+                </div>
+              ))}
+          </div>
+        </div>
     </>
   );
 };
